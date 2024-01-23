4824ca51f270535f992c14253a292d06a034c138
stwarHatLuffy <yadraj1803@gmail.com>
2024-01-23 11:05:31 -0500

    Added two category APIs -main

--- src/main/java/com/scaler/PSPractice/Controllers/CategoryController.java
+++ src/main/java/com/scaler/PSPractice/Controllers/CategoryController.java
@@ -1,10 +1,12 @@
 package com.scaler.PSPractice.Controllers;
 
 import com.scaler.PSPractice.DTOs.CategoryDTO;
+import com.scaler.PSPractice.DTOs.GenericProductDTO;
 import com.scaler.PSPractice.Exceptions.AlreadyExistsException;
 import com.scaler.PSPractice.Exceptions.NotFoundException;
 import com.scaler.PSPractice.Mappers.DTOMappers;
 import com.scaler.PSPractice.Models.Category;
+import com.scaler.PSPractice.Models.Product;
 import com.scaler.PSPractice.Services.CategoryService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
@@ -50,6 +52,16 @@ public class CategoryController {
     @PostMapping
     public CategoryDTO createCategory(@RequestBody CategoryDTO categoryDTO) throws AlreadyExistsException {
         return DTOMappers.categoryToCategoryResponseDTO(categoryService.createCategory(categoryDTO));
+    }
+
+    @GetMapping("/products/{id}")
+    public List<GenericProductDTO> getProductsByCategoryId(@PathVariable("id")String id) throws NotFoundException {
+       return categoryService.getAllProductsByCategoryId(id);
+    }
+
+    @GetMapping("/products")
+    public List<GenericProductDTO> getAllProductsByCategories(@RequestBody List<String> categoryIds){
+        return categoryService.getAllProductsByCategoryIds(categoryIds);
     }
 
 }
--- src/main/java/com/scaler/PSPractice/Models/Category.java
+++ src/main/java/com/scaler/PSPractice/Models/Category.java
@@ -3,6 +3,8 @@ package com.scaler.PSPractice.Models;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import java.util.List;
 
@@ -12,5 +14,6 @@ import java.util.List;
 public class Category extends BaseModel{
     private String name;
     @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+    @Fetch(FetchMode.SUBSELECT)
     private List<Product> products;
 }
